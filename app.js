c090d0e863d187aaa2a9342e48e7c98d8c7fc1ea
gkhan205 <ghazikhan205@gmail.com>
2017-01-17 10:38:07 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var bodyparser = require('body-parser');
+
+var app = express();
+
+port = 3000;
+
+app.use(express.static(__dirname + ('/public')));
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended: false}));
+
+app.get("/", function(req, res){
+	res.render('index.html');
+});
+
+app.get("/admin", function(req, res){
+	res.render('admin.html');
+});
+
+var user;
+
+app.get("/login", function(req, res){
+
+	var a = req.query.uname;
+	var b = req.query.pass;
+
+	console.log(a);
+	console.log(b);
+
+	if(a == 'admin' && b == 'admin'){
+		res.redirect('admin.html');
+	} else {
+		res.redirect('login.html');
+	}
+});	
+
+
+
+app.listen(port, function(){
+	console.log("Server running on Port No. "+port);
+});
